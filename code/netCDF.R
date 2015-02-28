33e1a8da29091c3e03f20cbc17283d8b5333bcc4
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2015-02-28 14:53:02 -0800

    updated meta data info in netCDF creation

--- code/netCDF.R
+++ code/netCDF.R
@@ -4,8 +4,8 @@ require(ncdf4)
 makeAlbersNetCDF <- function(name = NULL, units = '', longname = '', fn = NULL, dir = '', x, y, taxa, numSamples) {
   if(is.null(fn)) stop("makeAlbersNetCDF: requires filename as argument.")
 
-  x_dim <-  ncdim_def("x", "meters_east", x)
-  y_dim <-  ncdim_def("y", "meters_north", y)
+  x_dim <-  ncdim_def("x", "meters_east", x, longname = 'x coordinate of grid cell centroid in Albers projection (Great Lakes St Lawrence Albers [Proj4 +init=epsg:3175])')
+  y_dim <-  ncdim_def("y", "meters_north", y, longname = 'y coordinate of grid cell centroid in Albers projection (Great Lakes St Lawrence Albers [Proj4 +init=epsg:3175])')
   draw_dim <- ncdim_def("sample", "number", 1:numSamples, longname = "MCMC sample")
   vars <- list()
   length(vars) == length(taxa)
