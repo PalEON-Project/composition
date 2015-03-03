3dabdb0d0cbb42e7e7d4049c9618afea0c37a624
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2015-03-02 18:08:27 -0800

    fixed issues in plot_full

--- plot_full.R
+++ plot_full.R
@@ -22,9 +22,9 @@ usFortified <- fortify(usShp, region='id')
 ## get raw data in form to use
 ########################################################################################
 
-# don't I need rev(easternDomainY)?
-region = t(as.matrix(raster(file.path(dataDir, 'paleonDomain.tif'))))
-water = t(as.matrix(raster(file.path(dataDir, 'water.tif'))))
+# rev() flips from tif N->S orientation to netCDF S->N
+region = t(as.matrix(raster(file.path(dataDir, 'paleonDomain.tif'))))[ , yRes:1]
+water = t(as.matrix(raster(file.path(dataDir, 'water.tif'))))[ , yRes:1]
 # t()  manipulates matrix so plots correctly W-E and N-S in R
 
 # region[region %in% c(2,3,5,6,11,12)] <- NA
@@ -32,30 +32,54 @@ water[water == 100] <- NA
 mask = is.na(region)
 maskWater = is.na(water)
 
-# western data/results
+# western data
 
 load(file.path(dataDir, paste0('data_western_', runID, '.Rda')))
+taxa_west <- taxa
 
-rawWest <- matrix(0, nrow = nCells, ncol = nTaxa)
+raw_west <- matrix(0, nrow = nCells, ncol = nTaxa)
 for(p in 1:nTaxa) {
   tbl <- table(data$cell[data$taxon == p])
-  rawWest[as.numeric(names(tbl)) , p] <- tbl
+  raw_west[as.numeric(names(tbl)) , p] <- tbl
 }
-total <- rowSums(rawWest)
-rawWest[total == 0] = NA
+total <- rowSums(raw_west)
+raw_west[total == 0] = NA
 total[total == 0] <- 1
-rawWest <- rawWest / total
-dimnames(rawWest)[[2]] <- gsub("/", "ZZZ", taxa$taxonName)  # otherwise both / and " " become "." so can't distinguish when I substitute back in for "."
+raw_west <- raw_west / total
+dimnames(raw_west)[[2]] <- taxa$taxonName  
 
-finalNcdfName <- paste0('PLScomposition_western_', runID, '.nc')
+# eastern data
+load(file.path(dataDir, paste0('data_eastern_', runID, '.Rda')))
+taxa_east <- taxa
+load(file.path(dataDir, paste0('intersection_eastern_', productVersion, '.Rda')))
+
+nTowns <- dim(townCellOverlap)[1]
+easternDataDir <- 'eastern'
+ohioDataDir <- 'ohio'
+eastern_townships <- readOGR(file.path(dataDir, easternDataDir), paste0(easternVersionID, 'polygons_v', easternVersion))
+ohio_townships <- readOGR(file.path(dataDir, ohioDataDir), paste0('OH', ohioVersionID, 'polygons_v', ohioVersion))
+ohio_townships <- spTransform(ohio_townships, CRSobj=CRS('+init=epsg:3175'))  # transform to Albers
+
+raw_east <- matrix(0, nrow = nTowns, ncol = nTaxa)
+for(p in 1:nTaxa) {
+  tbl <- table(data$town[data$taxon == p])
+  raw_east[as.numeric(names(tbl)) , p] <- tbl
+}
+raw_east <- raw_east / rowSums(raw_east)
+attributes(raw_east)$dimnames[[2]] <- taxa$taxonName
+
+
+taxaNames <- unique(taxa_east$taxonName, taxa_west$taxonName)
+
+finalNcdfName <- paste0('composition_v', productVersion, '.nc')
 
 ncdfPtr <- nc_open(file.path(outputDir, finalNcdfName))
 test <- ncvar_get(ncdfPtr, "Oak", c(1, 1, 1), c(-1, -1, -1))
 
+nCells <- xRes*yRes
+nTaxa <- length(taxaNames)
 nSamples <- dim(test)[3]
 
-if(nCells != prod(dim(test)[1:2]))
-  stop("nCells does not match first dimension of netCDF file.")
 
 preds <- array(0, c(nCells, nTaxa, nSamples))
 #dimnames(preds)[[2]] <- taxa$taxonName
@@ -63,80 +87,16 @@ for(p in 1:nTaxa) {
   preds[ , p, ] <- ncvar_get(ncdfPtr, taxa$taxonName[p], c(1, 1, 1), c(-1, -1, -1))
 }
 
-attributes(preds)$dimnames[[2]] <- gsub("/", "ZZZ", taxa$taxonName) 
+attributes(preds)$dimnames[[2]] <- gsub("/", "ZZZ", taxaNames)
 
-pmWest <- apply(preds, c(1, 2), 'mean')
-psdWest <- apply(preds, c(1, 2), 'sd')
-
-
-
-# eastern data/results
-
-load(file.path(dataDir, paste0('data_eastern_', runID, '.Rda')))
-load(file.path(dataDir, paste0('intersection_eastern_', productVersion, '.Rda')))
-
-  
-
-finalNcdfName <- paste0('PLScomposition_eastern_', runID, '.nc')
-
-ncdfPtr <- nc_open(file.path(outputDir, finalNcdfName))
-test <- ncvar_get(ncdfPtr, "Oak", c(1, 1, 1), c(-1, -1, -1))
-
-nSamples <- dim(test)[3]
-
-nCells <- m1*m2
-
-if(nCells != prod(dim(test)[1:2]))
-  stop("nCells does not match first dimension of netCDF file.")
+pm <- apply(preds, c(1, 2), 'mean')
+psd <- apply(preds, c(1, 2), 'sd')
 
 
-preds <- array(0, c(nCells, nTaxa, nSamples))
-for(p in 1:nTaxa) {
-  preds[ , p, ] <- ncvar_get(ncdfPtr, taxa$taxonName[p], c(1, 1, 1), c(-1, -1, -1))
-}
+pm[mask, ] <- NA
+psd[mask, ] <- NA
 
-attributes(preds)$dimnames[[2]] <- gsub("/", "ZZZ", taxa$taxonName) 
-
-pmEast <- apply(preds, c(1, 2), 'mean')
-psdEast <- apply(preds, c(1, 2), 'sd')
-
-## combine across grids
-
-
-pmFull <- matrix(NA, c(xRes*yRes), ncol(pmEast))
-dimnames(pmFull)[[2]] <- dimnames(pmEast)[[2]]
-fullTmp <- psdFull <- rawFull <- pmFull
-
-ids <- matrix(1:(xRes*yRes), nrow = xRes, ncol = yRes)
-eastSubset <- c(ids[easternDomainX, easternDomainY])
-westSubset <- c(ids[westernDomainX, westernDomainY])
-
-eastOnly <- dimnames(pmEast)[[2]]
-eastOnly <- eastOnly[!(eastOnly %in% dimnames(pmWest)[[2]])]
-
-pmFull[eastSubset, ] <- pmEast
-fullTmp[westSubset, dimnames(pmWest)[[2]]] <- pmWest
-fullTmp[!(region %in% c(6,12,11, 5, 2, 3)), ] <- NA
-pmFull[!is.na(fullTmp)] <- fullTmp[!is.na(fullTmp)]
-pmFull[region %in% c(5, 12), eastOnly] <- NA
-
-psdFull[eastSubset, ] <- psdEast
-fullTmp[westSubset, dimnames(psdWest)[[2]]] <- psdWest
-fullTmp[!(region %in% c(6,12,11, 5, 2, 3)), ] <- NA
-psdFull[!is.na(fullTmp)] <- fullTmp[!is.na(fullTmp)]
-psdFull[region %in% c(5, 12), eastOnly] <- NA
-
-rawFull[westSubset, dimnames(rawWest)[[2]]] <- rawWest
-
-pmFull[mask, ] <- NA
-psdFull[mask, ] <- NA
-rawFull[mask, ] <- NA
-
-#tmp <- pmFull
-# tmp<tmp[1:296, 180:1]
-#image.plot(1:296,1:180, matrix(tmp,296,180))
-
-coordFull <- expand.grid(X = xGrid, Y = rev(yGrid))
+coord <- expand.grid(X = xGrid, Y = yGrid)
 
 #########################################################################################
 ## plot the data, plots in large grid
@@ -149,106 +109,139 @@ figWth = 22
 figHgtIndiv = 16*.5
 figWthIndiv = 22*.5
 
-pdf(file.path(outputDir, paste0('composition_', runID, '_fits.pdf')), height = figHgt, width = figWth)
-make_veg_map(data = pmFull, breaks = propBreaks, coords = coordFull, legendName = 'fitted proportions', map_data = usFortified, facet = TRUE)
+pdf(file.path(outputDir, paste0('composition_', productVersion, '_fits.pdf')), height = figHgt, width = figWth)
+make_veg_map(data = pm, breaks = propBreaks, coords = coord, legendName = 'fitted proportions', map_data = usFortified, facet = TRUE)
 dev.off()
 
-pdf(file.path(outputDir, paste0('composition_', runID, '_fits_indiv.pdf')), height = figHgtIndiv, width = figWthIndiv)
-make_veg_map(data = pmFull, breaks = propBreaks, coords = coordFull, legendName = 'fitted proportions', map_data = usFortified, facet = FALSE)
+pdf(file.path(outputDir, paste0('composition_', productVersion, '_fits_indiv.pdf')), height = figHgtIndiv, width = figWthIndiv)
+make_veg_map(data = pm, breaks = propBreaks, coords = coord, legendName = 'fitted proportions', map_data = usFortified, facet = FALSE)
 dev.off()
 
-psdFull[psdFull > .25] = 0.25
+psd[psd > .25] = 0.25
 psdBreaks = c(0, 0.01, 0.03, 0.05, 0.075, 0.10, 0.15, 0.2, 0.25)
 #psd[psd > .3] = 0.3
 #psdBreaks = c(0, 0.01, 0.03, 0.05, 0.10, 0.15, 0.2, 0.25, 0.3)
   
-pdf(file.path(outputDir, paste0('composition_', runID, '_uncertainty.pdf')), height = figHgt, width = figWth)
-make_veg_map(data = psdFull, breaks = psdBreaks, coords = coordFull, legendName = 'std. error', map_data = usFortified, col = heat.colors, facet = TRUE)
+pdf(file.path(outputDir, paste0('composition_', productVersion, '_uncertainty.pdf')), height = figHgt, width = figWth)
+make_veg_map(data = psd, breaks = psdBreaks, coords = coord, legendName = 'std. error', map_data = usFortified, col = heat.colors, facet = TRUE)
 dev.off()
 
-pdf(file.path(outputDir, paste0('composition_full_', runID, '_uncertainty_indiv.pdf')), height = figHgtIndiv, width = figWthIndiv)
-make_veg_map(data = psdFull, breaks = psdBreaks, coords = coordFull, legendName = 'std. error', map_data = usFortified, col = heat.colors, facet = FALSE)
+pdf(file.path(outputDir, paste0('composition_', productVersion, '_uncertainty_indiv.pdf')), height = figHgtIndiv, width = figWthIndiv)
+make_veg_map(data = psd, breaks = psdBreaks, coords = coord, legendName = 'std. error', map_data = usFortified, col = heat.colors, facet = FALSE)
 dev.off()
 
 
-# this is not working and not sure exactly how to combine the raster info in West with polygon info in east - probably convert rasters to polygons and then combine the polygons
-# actually (11/22/14) I figured that out and code is in paper.R in the composition_paper folder
+# rawData plots
 
-if(FALSE) { 
-# plot raw data as colored polygons
-# it would be nice to have this as a function, but there is a lot of pre-processing...
+make_areal_map <- function(data, variables = NULL, breaks, legendName = 'Raw proportions', map_data = usShp, facet = TRUE, col = terrain.colors, zero_color = terrain.colors(40)[39], reverse_colors = TRUE, print = TRUE, ncol = 5, legend = TRUE) {
+  make_map <- function(p) {
 
-# west
-
-  data_binned <- matrix(0, nrow = nrow(rawWest), ncol = ncol(rawWest))
-  colnames(data_binned) <- colnames(rawWest)
-  
-  for (p in seq_len(ncol(data))) {
-    data_binned[ , p] <- cut(rawWest[ , p], breaks, include.lowest = TRUE, labels = FALSE)
+    if(!is.null(p)) {
+        nm <- p
+        nm <- gsub("ZZZ", "/", nm)
+        nm <- gsub("\\.", " ", nm)
+    }
+    
+    col <- col(length(breaks)-1)
+    if(reverse_colors) col <- rev(col)
+    if(legend) guide <- "legend" else guide <- "none"
+    col[1] <- zero_color
+    d <- ggplot(data_to_plot, aes(X, Y, group = id)) + geom_polygon(aes(fill = as.factor(value))) + 
+      scale_fill_manual(values = col, labels = breaklabels, name = legendName, guide = guide) +
+        theme(strip.text.x = element_text(size = 16), legend.key.size = unit(1.5, "cm"), legend.text = element_text(size = 16), legend.title = element_text(size = 16))
+    d <- add_map_albers(plot_obj = d, map_data = map_data, dat = data_to_plot)
+    # this makes sure eastern-only taxa have full map
+    d <- d + scale_y_continuous(limits = range(yGrid)) + scale_x_continuous(limits = range(xGrid))
+    if(facet) {
+      d <- d + facet_wrap(~variable, ncol = ncol)
+    } else {
+      d <- d + ggtitle(nm)
+    }
+    d <- theme_clean(d)
+    return(d)
   }
-    taxon_dat <- data.frame(X = coordsWest$X, Y = coordsWest$Y, data_binned)
-    taxon_dat_long_west <- melt(taxon_dat, c('X', 'Y'))
 
-# east
-
-
-nTowns <- dim(townCellOverlap)[1]
-easternDataDir <- paste0(easternVersionID, '.', easternVersion)
-ohioDataDir <- paste0(ohioVersionID, '.', ohioVersion)
-eastern_townships <- readOGR(file.path(dataDir, easternDataDir), paste0(easternVersionID, 'polygonsver', easternVersion))
-ohio_townships <- readOGR(file.path(dataDir, ohioDataDir), paste0('OH', ohioVersionID, 'polygonsver', ohioVersion))
-ohio_townships <- spTransform(ohio_townships, CRSobj=CRS('+init=epsg:3175'))  # transform to Albers
-
-raw <- matrix(0, nrow = nTowns, ncol = nTaxa)
-for(p in 1:nTaxa) {
-  tbl <- table(data$town[data$taxon == p])
-  raw[as.numeric(names(tbl)) , p] <- tbl
-}
-raw <- raw / rowSums(raw)
-attributes(raw)$dimnames[[2]] <- gsub("/", "ZZZ", taxa$taxonName) 
-
-for(p in seq_len(nTaxa))
-  raw[ , p] <- cut(raw[ , p], propBreaks, include.lowest = TRUE, labels = FALSE)
-
-east_fort <- fortify(eastern_townships)
-ohio_fort <- fortify(ohio_townships)
-
-idMap = c(eastern_townships@data$ID, ohio_townships@data$ID + 1 + length(eastern_townships))
-# this is dangerous as it relies on data$town corresponding to $id+1 values where $id is from the @polygons 
-east_fort$id = as.numeric(east_fort$id) + 1
-ohio_fort$id = as.numeric(ohio_fort$id) + 1 + length(eastern_townships)
-fort <- rbind(east_fort, ohio_fort)
-fort$id = idMap[fort$id]
-fort <- cbind(fort, raw[fort$id,])
-names(fort)[1:2] <- c('X', 'Y')
-
-#ggplot(fort, aes(long, lat, group = id)) + geom_polygon(aes(fill = Oak))
-
-names(fort) <- gsub("ZZZ", "/", names(fort))
-taxon_dat_long <- melt(fort, c('X', 'Y', 'order', 'hole', 'piece', 'group', 'id' ))
-
-breaks = propBreaks
-breaklabels <- apply(cbind(breaks[1:(length(breaks)-1)], breaks[2:length(breaks)]), 1, 
+  if(!is.null(variables))
+    data <- data[data$variable %in% variables, ]
+  data$value <- cut(data$value, breaks, include.lowest = TRUE, labels = FALSE)
+  
+  breaklabels <- apply(cbind(breaks[1:(length(breaks)-1)], breaks[2:length(breaks)]), 1, 
                        function(r) { sprintf("%0.2f - %0.2f", r[1], r[2]) })
 
-legendName = "raw proportions"
+  if(facet) {
+    data_to_plot <- data
+    d <- make_map(NULL)
+    if(print) print(d)
+  } else {
+    for ( p in variables ) {
+        data_to_plot <- data[data$variable == p, ]
+        d <- make_map(p)
+        if(print) print(d)
+    }
+  }
+  invisible(d)
+}
 
-# make_veg_map(data = rawFull, breaks = propBreaks, coords = coord, map_data = usFortified, legendName = 'raw proportions', facet = TRUE)
 
-# ggplot(taxon_dat_long, aes(X, Y, group = id)) + geom_polygon(aes(fill = as.factor(value)))
+raw_west_poly <- rasterToPolygons(raster(ncols = length(westernDomainX), nrows = length(westernDomainY),
+                                       xmn = xRange[1], xmx = easternLimitOfWesternDomain + gridSpacing/2,
+                         ymn = yRange[1], ymx = yRange[2]),
+                      fun=NULL, n=4, na.rm=TRUE, digits=12, dissolve=FALSE)
+west_fort <- fortify(raw_west_poly)
+names(west_fort)[1:2] <- c('X', 'Y')
 
-d <- ggplot(taxon_dat_long, aes(X, Y, group = id)) +
-  geom_polygon(aes(fill = as.factor(value))) + 
-  scale_fill_manual(values = tim.colors(length(breaks)), labels = breaklabels, name = legendName) +
-  theme(strip.text.x = element_text(size = 16), legend.key.size = unit(1.5, "cm"), legend.text = element_text(size = 16), legend.title = element_text(size = 16)) +
-  coord_fixed() +
-  geom_raster(data = taxon_dat_long_west, aes(x = X, y = Y, fill = factor(value))) +
-  facet_wrap( ~ variable, ncol=5)
-d <- add_map_albers(plot_obj = d, map_data = usFortified, dat = taxon_dat_long)
-d <- theme_clean(d)
+# put NA for masked areas for taxa not modeled in west
+taxaMissingInWest <- !(taxaNames %in% dimnames(raw_west)[[2]])
+old_raw_west <- raw_west
+raw_west <- matrix(as.numeric(NA), nrow(old_raw_west), ncol(raw_east))
+dimnames(raw_west)[[2]] <- taxaNames
+# only really needed if want to plot 0 instead of NA where data are present:
+raw_west[mask[westernDomainX, rev(westernDomainY)], ] <- NA
+# chestnut/atl wh cedar in west get stripped out later anyway
+# (otherwise hard to deal with 0 tree places in MN and with
+# ohio overlap
+raw_west[ , dimnames(old_raw_west)[[2]]] <- old_raw_west
 
-pdf(file.path(outputDir, paste0('PLScomposition_eastern_', runID, '_rawData.pdf')), height = figHgt, width = figWth)
-print(d)
+west_fort$id <- as.numeric(west_fort$id)
+west_fort <- cbind(west_fort, raw_west[west_fort$id, ])
+
+northeast_fort <- fortify(eastern_townships)
+northeast_fort$id <- as.numeric(northeast_fort$id)
+ohio_fort <- fortify(ohio_townships)
+ohio_fort$id <- as.numeric(ohio_fort$id)
+
+northeast_idMap <- data.frame(id = sort(eastern_townships@data$ID), town = seq_along(eastern_townships))
+ohio_idMap <- data.frame(id = sort(ohio_townships@data$ID), town = length(eastern_townships) + seq_along(ohio_townships))
+
+northeast_fort <- merge(northeast_fort, northeast_idMap, by.x = 'id', by.y = 'id', all.x = TRUE, all.y = FALSE)
+northeast_fort <- northeast_fort[order(as.numeric(northeast_fort$id), northeast_fort$order), ]
+
+ohio_fort <- merge(ohio_fort, ohio_idMap, by.x = 'id', by.y = 'id', all.x = TRUE, all.y = FALSE)
+ohio_fort <- ohio_fort[order(as.numeric(ohio_fort$id), ohio_fort$order), ]
+
+east_fort <- rbind(northeast_fort, ohio_fort)
+east_fort <- cbind(east_fort, raw_east[east_fort$town,])
+east_fort$id <- east_fort$town + max(west_fort$id)
+names(east_fort)[2:3] <- c('X', 'Y')
+east_fort$town <- NULL
+east_fort <- east_fort[ , names(west_fort)]
+
+fort <- rbind(west_fort, east_fort)
+
+
+#names(fort) <- gsub("ZZZ", "/", names(fort))
+taxon_dat_long <- melt(fort, c('X', 'Y', 'order', 'hole', 'piece', 'group', 'id' ))
+
+max_west_id <- max(west_fort$id)
+taxon_dat_long <- taxon_dat_long[!(taxon_dat_long$id <= max_west_id & taxon_dat_long$variable %in% taxaNames[taxaMissingInWest]), ]
+
+#  figs[[cnt]] <- make_areal_map(data = taxon_dat_long, variables = taxon, breaks = propBreaks, legendName = 'raw proportions', map_data = usFortified, facet = FALSE, ncol = 1, legend = FALSE) + theme(plot.margin = unit(rep(0,4), 'lines'))
+
+pdf(file.path(outputDir, paste0('composition_', productVersion, '_rawData.pdf')), height = figHgt, width = figWth)
+make_areal_map(data = taxon_dat_long, variables = taxaNames, breaks = propBreaks, legendName = 'raw proportions', map_data = usFortified, facet = TRUE, legend = FALSE)
 dev.off()
 
-}
+pdf(file.path(outputDir, paste0('composition_', productVersion, '_rawData_indiv.pdf')), height = figHgtIndiv, width = figWthIndiv)
+make_areal_map(data = taxon_dat_long, variables = taxaNames, breaks = propBreaks, legendName = 'raw proportions', map_data = usFortified, facet = FALSE, legend = TRUE)
+dev.off()
+
